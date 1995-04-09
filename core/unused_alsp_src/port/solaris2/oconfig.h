6f4e39a75855cd8dde6da3b8eeb50a8d8a756930
Ken Bowen <kbowen@applied-logic-systems.com>
1995-04-09 19:22:24 +0000

    Misc; version = v1-71C

---
+++ core/unused_alsp_src/port/solaris2/oconfig.h
@@ -0,0 +1,9 @@
+/*=======================================================
+ |		oconfig.h
+ |		Copyright (c) 1995 Applied Logic Systems, Inc.
+ |
+ |		OS-specific config file for SOLARIS 2.*
+ *======================================================*/
+
+#define UNIX	1
+#define OSStr "unix"
