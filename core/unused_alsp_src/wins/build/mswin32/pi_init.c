798b1ec913e80b6b0054e3f3d4db99fa1c3a798a
Chuck Houpt <chuck@habilis.net>
1996-04-02 20:45:47 +0000

    mswin32 files

---
+++ core/unused_alsp_src/wins/build/mswin32/pi_init.c
@@ -0,0 +1,26 @@
+/*=====================================================================*
+ |		pi_init.c
+ | Copyright (c) 1988-1996, Applied Logic Systems, Inc.
+ |
+ |	- Foreign interface initialization
+ |		- a segment of the old pimain.c
+ *=====================================================================*/
+
+#include "pi_init.h"
+
+extern void kernal32_init(void);
+extern void user32_init(void);
+extern void gdi32_init(void);
+extern void mswin32aux_init(void);
+
+void pi_init(void)
+{
+	kernal32_init();
+	user32_init();
+	gdi32_init();
+	winspool32_init();
+	shell32_init();
+	//ole32_init();
+	comdlg32_init();
+	mswin32aux_init();
+}
