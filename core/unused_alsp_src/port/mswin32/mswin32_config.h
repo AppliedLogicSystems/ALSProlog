f6bfc2b7f2fc657139c3060a4bf7f67b29cfc9ab
Chuck Houpt <chuck@habilis.net>
1996-04-02 21:27:06 +0000

    changed directory from mswin95 to mswin32

---
+++ core/unused_alsp_src/port/mswin32/mswin32_config.h
@@ -0,0 +1,56 @@
+/* 
+ *  mswin32_config.h	Hand made MSWin32 configuration file.
+ *	Copyright (c) 1996 Applied Logic Systems, Inc.
+ *
+ *  Author:	Chuck Houpt
+ *  Creation:	1/30/96
+ *  Revision History:
+ *	Revised: mm/dd/yy	Who		-- Why and What
+ */
+
+#define HAVE_STDARG_H	1
+#define HAVE_STDLIB_H	1
+#define HAVE_FCNTL_H	1
+#define HAVE_STRING_H	1
+#define HAVE_SRAND		1
+
+#define HAVE_TIME		1
+
+#define HAVE_SOCKET		1
+#define BERKELEY_SOCKETS	1
+#define HAVE_SELECT		1
+
+#define PARAMREVBIT 1
+
+#define APP_PRINTF_CALLBACK	1
+//#undef FSACCESS
+
+#define SIMPLE_MICS	1
+#if 0
+
+#define MISSING_EXTERN_ACCESS	1
+#define MISSING_EXTERN_CHDIR	1
+#define MISSING_EXTERN_GETCWD	1
+#define MISSING_EXTERN_LSEEK	1
+#define MISSING_EXTERN_OPEN		1
+#define MISSING_EXTERN_READ		1
+#define MISSING_EXTERN_WRITE	1
+#define MISSING_EXTERN_CLOSE	1
+#define MISSING_EXTERN_UNLINK	1
+
+#define STDC_HEADERS	1
+
+
+#define HAVE_STDDEF_H	1
+
+
+#define HAVE_MEMMOVE	1
+
+
+#define HAVE_STRCSPN	1
+#define HAVE_STRSPN	1
+#define HAVE_STRTOK	1
+
+
+#define HAVE_VFPRINTF	1
+#endif
---
+++ core/unused_alsp_src/port/mswin32/oconfig.h
@@ -0,0 +1,9 @@
+/*=======================================================
+ |		oconfig.h
+ |		Copyright (c) 1995 Applied Logic Systems, Inc.
+ |
+ |		OS-specific config file for Microsoft Win32
+ *======================================================*/
+
+#define MSWin32	1
+#define OSStr "mswin32"
