53bede9bec00f59cddde7aa585e0e335a07a1c6c
Chuck Houpt <chuck@habilis.net>
1996-04-04 22:24:34 +0000

    further updates

--- core/unused_alsp_src/wins/build/mswin32/do_c2pro.bat
+++ core/unused_alsp_src/wins/build/mswin32/do_c2pro.bat
@@ -1,6 +1,7 @@
 set ALS_OPTIONS=heap_size:20000,stack_size:15192
-set C=%ALSP_SRC%\cinterf\c2pro
+set C=%ALSP_SRC%\cinterf\c2pro\
 copy %C%c2p.pro + %C%c2pro + %C%cexp + %C%cfiles + %C%cmacro + %C%cout + %C%cparse + %C%ctoken c2prolcl.pro
 set C=
-..\bld-port\alspro -ind c2procmd.lst %1 -srcpath %ALSP_SRC% -filterFile %ALSP_SRC%\wins\src\mswin32\%1.filter 
+..\..\bld-port\alspro_b -ind c2p_cmd.lst %1 -srcpath %ALSP_SRC%\wins\src\mswin32\ -filterFile %ALSP_SRC%\wins\src\mswin32\%1.filter 
 set ALS_OPTIONS=
+rem %ALSP_SRC%\wins\src\mswin32\
--- core/unused_alsp_src/wins/build/mswin32/make_interf.bat
+++ core/unused_alsp_src/wins/build/mswin32/make_interf.bat
@@ -4,9 +4,9 @@ call do_c2pro user32
 call do_p2intf user32
 call do_c2pro gdi32
 call do_p2intf gdi32
-call do_c2pro shell32
-call do_p2intf shell32
+rem call do_c2pro shell32
+rem call do_p2intf shell32
 call do_c2pro winspool32
 call do_p2intf winspool32
-call do_c2pro commdlg32
-call do_p2intf commdlg32
+call do_c2pro comdlg32
+call do_p2intf comdlg32
