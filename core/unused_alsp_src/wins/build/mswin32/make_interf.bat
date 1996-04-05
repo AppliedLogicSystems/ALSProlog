5ec9d4920734619a6cb1d433c4b45b773e139abe
Chuck Houpt <chuck@habilis.net>
1996-04-05 00:40:10 +0000

    added shell32 back in

--- core/unused_alsp_src/wins/build/mswin32/make_interf.bat
+++ core/unused_alsp_src/wins/build/mswin32/make_interf.bat
@@ -4,8 +4,8 @@ call do_c2pro user32
 call do_p2intf user32
 call do_c2pro gdi32
 call do_p2intf gdi32
-rem call do_c2pro shell32
-rem call do_p2intf shell32
+call do_c2pro shell32
+call do_p2intf shell32
 call do_c2pro winspool32
 call do_p2intf winspool32
 call do_c2pro comdlg32
