ffdd20ce9cddcde16ad130a350790ef99bb14635
Chuck Houpt <chuck@habilis.net>
1996-04-18 20:17:29 +0000

    Converted to unix text files

--- core/unused_alsp_src/wins/src/mswin32/ole32.h
+++ core/unused_alsp_src/wins/src/mswin32/ole32.h
@@ -1,1 +1,25 @@
-/*=====================================================================* | |	ole32.h |	Copyright (c) 1994-96 Applied Logic Systems, Inc. | *=====================================================================*//* Stop Inline Assembly. */#define NO_INLINE_FUNCTIONS 1//#include <windef.h>//#include <winbase.h>//#include <wingdi.h>//#include <ole2.h>#include <windows.h>/* PROCESSOR_ARCHITECTURE_UNKNOWN is defined as 0xFFFF;   The semi-colon is probably just a typo, so redefine it here. */ #ifdef PROCESSOR_ARCHITECTURE_UNKNOWN#undef PROCESSOR_ARCHITECTURE_UNKNOWN#define PROCESSOR_ARCHITECTURE_UNKNOWN 0xFFFF#endif
+/*=====================================================================*
+ |
+ |	ole32.h
+ |	Copyright (c) 1994-96 Applied Logic Systems, Inc.
+ |
+ *=====================================================================*/
+
+/* Stop Inline Assembly. */
+#define NO_INLINE_FUNCTIONS 1
+
+//#include <windef.h>
+//#include <winbase.h>
+//#include <wingdi.h>
+
+//#include <ole2.h>
+
+#include <windows.h>
+
+/* PROCESSOR_ARCHITECTURE_UNKNOWN is defined as 0xFFFF;
+   The semi-colon is probably just a typo, so redefine it here. */
+ 
+#ifdef PROCESSOR_ARCHITECTURE_UNKNOWN
+#undef PROCESSOR_ARCHITECTURE_UNKNOWN
+#define PROCESSOR_ARCHITECTURE_UNKNOWN 0xFFFF
+#endif
