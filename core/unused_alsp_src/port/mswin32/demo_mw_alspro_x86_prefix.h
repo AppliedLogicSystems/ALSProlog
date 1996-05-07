5c7b46596e697f3c536cdaa179d785986829ecea
Chuck Houpt <chuck@habilis.net>
1996-05-07 21:12:43 +0000

    added demo versions

---
+++ core/unused_alsp_src/port/mswin32/demo_mw_alspro_x86_prefix.h
@@ -0,0 +1,3 @@
+#include "x86_prefix.h"
+#define HAVE_CONFIG_H
+#define SSI_UNIKEY
--- core/unused_alsp_src/port/mswin32/package.bat
+++ core/unused_alsp_src/port/mswin32/package.bat
@@ -1,2 +1,2 @@
 .\alspro_b -obp -g save_image('alspro.exe',[select_lib(builtins,[debugger])])
-
+if exist demo_alspro_b.exe .\demo_alspro_b -obp -g save_image('demo_alspro.exe',[select_lib(builtins,[debugger])])
