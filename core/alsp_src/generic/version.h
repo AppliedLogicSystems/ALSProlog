70e6f4283595ead508a447c9067281cc1846b1db
Chuck Houpt <chuck@habilis.net>
1996-05-13 16:54:55 +0000

    upped to 2.02

--- core/alsp_src/generic/version.h
+++ core/alsp_src/generic/version.h
@@ -5,5 +5,5 @@
  *          Version number - Thu Apr 4 09:28:36 EST 1996
  *===============================================================*/
 #ifndef SysVersionNum
-#define SysVersionNum "2.01"
+#define SysVersionNum "2.02"
 #endif /* SysVersionNum */
