6f36a39f258e918416d5b2ed13f199a010d1f916
Chuck Houpt <chuck@habilis.net>
1996-05-16 17:02:09 +0000

    Added -b

--- core/unused_alsp_src/wins/build/mswin32/do_p2intf.bat
+++ core/unused_alsp_src/wins/build/mswin32/do_p2intf.bat
@@ -1,5 +1,5 @@
 set ALS_OPTIONS=heap_size:24000,stack_size:16192
 set C=%ALSP_SRC%\cinterf\pro2intf\
 copy %C%p2i.pro + %C%pro2intf + %C%intfout + %C%mytrans p2intlcl.pro
-..\..\bld-port\alspro -ind p2i_cmd.lst %1 -fpre %1
+..\..\bld-port\alspro -b -ind p2i_cmd.lst %1 -fpre %1
 set ALS_OPTIONS=
