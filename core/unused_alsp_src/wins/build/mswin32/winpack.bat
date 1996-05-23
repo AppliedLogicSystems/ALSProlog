6e278f1487e8c4e3d6f997aee66f6511162ab3ca
Chuck Houpt <chuck@habilis.net>
1996-05-23 16:36:40 +0000

    made winpack a batch (-b) call

--- core/unused_alsp_src/wins/build/mswin32/winpack.bat
+++ core/unused_alsp_src/wins/build/mswin32/winpack.bat
@@ -1,2 +1,2 @@
-alspro_mswin320 kernal32.pro user32.pro gdi32.pro comdlg32.pro winspool32.pro -g save_image('alspro_mswin32.exe',[select_lib(builtins,[debugger])])
+alspro_mswin320 -b kernal32.pro user32.pro gdi32.pro comdlg32.pro winspool32.pro -g save_image('alspro_mswin32.exe',[select_lib(builtins,[debugger])])
 
