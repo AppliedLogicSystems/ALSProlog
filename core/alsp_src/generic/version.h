7f8dae2b8eb9351a22d91117328990d24cf3f72c
Chuck Houpt <chuck@habilis.net>
1996-08-28 22:54:39 +0000

    Start of development for ALS Prolog 2.06

--- core/alsp_src/generic/version.h
+++ core/alsp_src/generic/version.h
@@ -5,5 +5,5 @@
  *          Version number - Thu Apr 4 09:28:36 EST 1996
  *===============================================================*/
 #ifndef SysVersionNum
-#define SysVersionNum "2.05"
+#define SysVersionNum "2.06 dev"
 #endif /* SysVersionNum */
