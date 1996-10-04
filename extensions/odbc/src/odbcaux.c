1730a0f69c7e19331199a24816e1348c28ba05b7
Chuck Houpt <chuck@habilis.net>
1996-10-04 22:34:50 +0000

    Further modifications

--- extensions/odbc/src/odbcaux.c
+++ extensions/odbc/src/odbcaux.c
@@ -1,1 +1,37 @@
-/*=====================================================================* | |	odbcaux.c |	Copyright (c) 1996 Applied Logic Systems, Inc. | *=====================================================================*/#include "alspi.h"#include "cinterf.h"#include <windows.h>static int pbi_GetForegroundWindow(void){	PWord arg1, rval;	int type1, rtype;			PI_getan(&arg1,&type1,1);		PI_makedouble(&rval,&rtype,(double) (long) GetForegroundWindow());	if (!PI_unify(arg1,type1,rval,rtype)) PI_FAIL;	PI_SUCCEED;}PI_BEGIN	PI_PDEFINE("o_GetForegroundWindow", 1, pbi_GetForegroundWindow, "_pbi_GetForegroundWindow")PI_ENDvoid odbcaux_init(void);void odbcaux_init(void){	PI_INIT;}
+/*=====================================================================*
+ |
+ |	odbcaux.c
+ |	Copyright (c) 1996 Applied Logic Systems, Inc.
+ |
+ *=====================================================================*/
+
+#include "alspi.h"
+#include "cinterf.h"
+
+#ifdef WIN32
+#include <windows.h>
+
+static int pbi_GetForegroundWindow(void)
+{
+	PWord arg1, rval;
+	int type1, rtype;
+		
+	PI_getan(&arg1,&type1,1);
+	
+	PI_makedouble(&rval,&rtype,(double) (long) GetForegroundWindow());
+	if (!PI_unify(arg1,type1,rval,rtype)) PI_FAIL;
+	PI_SUCCEED;
+}
+#endif
+
+PI_BEGIN
+#ifdef WIN32
+	PI_PDEFINE("o_GetForegroundWindow", 1, pbi_GetForegroundWindow, "_pbi_GetForegroundWindow")
+#endif
+PI_END
+
+void odbcaux_init(void);
+void odbcaux_init(void)
+{
+	PI_INIT;
+}
