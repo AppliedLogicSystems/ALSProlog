0296d1863ef20f5ea370dc48692a260734a570e0
Chuck Houpt <chuck@habilis.net>
1996-11-22 23:14:34 +0000

    added functions missing from MW Standard Libs, CW10 header update

--- core/alsp_src/generic/fswin32.c
+++ core/alsp_src/generic/fswin32.c
@@ -3,6 +3,9 @@
 #ifdef MSWin32
 
 #include <windows.h>
+#include <errno.h>
+
+#include "fswin32.h"
 
 /*
  * $getDirEntries/3 (--> getDirEntries/3 )
@@ -86,4 +89,181 @@ getDirEntries()
         
     PI_SUCCEED;     
 }
+
+#ifdef __MWERKS__
+int als_access(const char *path, int mode)
+{
+	/* ALS MOD */
+    DWORD attributes;
+    int result;
+    
+    attributes = GetFileAttributes(path);
+ 
+	if (attributes == 0xFFFFFFFF) {
+		if (GetLastError() == ERROR_INVALID_ACCESS) errno = EACCES;
+		else errno = ENOENT;
+		result = -1;
+	} else {
+		if (mode & W_OK && attributes & FILE_ATTRIBUTE_READONLY) {
+			errno = EACCES;
+			result = -1;
+		} else {
+			errno = ENOERR;
+			result = 0;
+		}
+	}
+	
+	return result;
+}
+
+/*** ALS MOD - derived from __get_time() in time.win32.c ***/
+static time_t FileTimeToTime_T(const FILETIME *fileTime)
+{
+	SYSTEMTIME time;
+	long years, leap_years, leap_days, days, i, secs;
+	
+	static int MonthDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+							//jan feb mar apr may jun jul aug sep oct nov dec
+
+	// First, convert file time to system time
+
+	FileTimeToSystemTime(fileTime, &time);
+
+	// Calculate the number of full years
+
+	years = time.wYear - 1900;
+
+	// Calculate the number of leap years in that time
+
+	leap_years = years;
+	if (time.wMonth > 2)
+	{
+	    leap_years++;
+	}
+	leap_days = (leap_years + 1) / 4;
+
+
+	// Calculate total days
+
+	days = (years * 365) + leap_days;
+
+	// Calculate days in this year
+
+	for (i=1; i<time.wMonth; i++)
+	{
+	    days += MonthDays[i-1];
+	}
+	days += time.wDay - 1;
+	
+	// and calculate the seconds
+
+	secs =  (((((days * 24) + time.wHour) * 60) + time.wMinute) * 60) + time.wSecond;
+	
+	return secs;
+
+}
+
+/*
+ *	int stat(char *path, struct stat *buf)
+ *
+ *		Returns information about a file.
+ */
+int als_stat(const char *path, struct als_stat *buf)
+{
+/*** ALS MOD ***/
+	HANDLE findHandle;
+	WIN32_FIND_DATA findFileData;
+	int result;
+	
+	if (path) {
+		findHandle = FindFirstFile(path, &findFileData);
+		
+		if (findHandle != INVALID_HANDLE_VALUE) {
+			
+			if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+				buf->st_mode = S_IFDIR;
+				buf->st_nlink = 2;
+			} else {
+				buf->st_mode = S_IFREG;
+				buf->st_nlink = 1;
+			}
+			
+			buf->st_ino = 0;
+			buf->st_dev = 0;
+			buf->st_uid = 0;
+			buf->st_gid = 0;
+			buf->st_rdev = 0;
+			buf->st_size = findFileData.nFileSizeHigh ? MAXDWORD : findFileData.nFileSizeLow;
+			buf->st_atime = FileTimeToTime_T(&findFileData.ftLastAccessTime);
+			buf->st_mtime = FileTimeToTime_T(&findFileData.ftLastWriteTime);
+			buf->st_ctime = FileTimeToTime_T(&findFileData.ftCreationTime);
+			buf->st_blksize = 0;
+			buf->st_blocks = 0;
+			
+			if (FindClose(findHandle)) result = 0;
+			else result = -1;
+			
+		} else result = -1;
+		
+	} else result = -1;
+
+	return result;
+}
+
+char * als_getenv(const char * name)
+{
+	#define VAL_MAX	1024
+	static TCHAR value[VAL_MAX];
+	DWORD result;
+	
+	result = GetEnvironmentVariable(name, value, VAL_MAX);
+	
+	if (result <= 0 || result > VAL_MAX) return NULL;
+	else return value;
+}
+
+int als_system(const char * command)
+{
+/* Bug - this doesn't work from a network.  It looks like COMMAND.COM is
+   unhappy with working directories like \\machine\share\dir.  I think
+   this is something for MicroSoft to fix. */
+	#define COM_MAX 1024
+	TCHAR commandLine[COM_MAX] = "COMMAND.COM";
+	TCHAR title[MAX_PATH];
+	DWORD got_title;
+	STARTUPINFO startInfo;
+	PROCESS_INFORMATION procInfo;
+	DWORD result;
+    
+	GetEnvironmentVariable("COMSPEC", commandLine, COM_MAX);
+    
+	strncat(commandLine, " /C ", COM_MAX-1);
+    
+	strncat(commandLine, command, COM_MAX-1);
+
+	startInfo.cb = sizeof(STARTUPINFO);
+	startInfo.lpReserved = NULL;
+	startInfo.lpDesktop = NULL;
+	startInfo.lpTitle = NULL;
+	startInfo.dwFlags = 0;
+	startInfo.cbReserved2 = 0;
+	startInfo.lpReserved2 = NULL;
+
+	/* Some commands (for example Unix95's ls) change the console title without
+	   restoring it, so lets save and restore the title. */
+	got_title = GetConsoleTitle(title, MAX_PATH);
+
+	if (!CreateProcess(NULL, commandLine, NULL, NULL, TRUE, 0, NULL, NULL,
+		&startInfo, &procInfo)) return 0;
+
+	if (WaitForSingleObject(procInfo.hProcess, INFINITE) != WAIT_OBJECT_0
+		|| !GetExitCodeProcess(procInfo.hProcess, &result)) result = 0;
+
+	if (got_title) SetConsoleTitle(title);
+
+	return result;
+}
+
+#endif
+
 #endif
--- core/alsp_src/generic/fswin32.h
+++ core/alsp_src/generic/fswin32.h
@@ -1,12 +1,41 @@
 /* MSWin32 CRT headers. */
 
+#ifdef __MWERKS__
 #include <unix.h>
 
 /* There are no symbolic links in Win32, so lstat == stat.
    But what about Win95 aliases? */
 #define lstat stat
 
-#define access _access
+/* unistd.win32.h is missing these defines */
+#define R_OK 1
+#define W_OK 2
+#define X_OK 4
+#define F_OK 8
+
+int als_access(const char *path, int mode);
+#define access als_access
+
+
+struct als_stat
+{
+	mode_t		st_mode;		/* File mode; see #define's below */
+	ino_t		st_ino;			/* File serial number */
+	dev_t		st_dev;			/* ID of device containing this file */
+	nlink_t		st_nlink;		/* Number of links */
+	uid_t		st_uid;			/* User ID of the file's owner */
+	gid_t		st_gid;			/* Group ID of the file's group */
+	dev_t		st_rdev;		/* Device type */
+	off_t		st_size;		/* File size in bytes */
+	time_t		st_atime;		/* Time of last access */
+	time_t		st_mtime;		/* Time of last data modification */
+	time_t		st_ctime;		/* Time of last file status change */
+	long		st_blksize;		/* Optimal blocksize */
+	long		st_blocks;		/* blocks allocated for file */
+};
+
+int als_stat(const char *path, struct als_stat *buf);
+#define stat als_stat
 
 #define EINTR	3001
 
@@ -16,3 +45,11 @@
 #define S_IRUSR	0
 #define S_IWUSR	0
 #define S_IXUSR	0
+
+char * als_getenv(const char * name);
+int als_system(const char * command);
+#define getenv als_getenv
+#define system als_system
+
+
+#endif
