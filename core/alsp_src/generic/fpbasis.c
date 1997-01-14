90e437e78dba6502a5af23835b97e2c2d526da26
Ken Bowen <kbowen@applied-logic-systems.com>
1997-01-14 17:32:40 +0000

    IEEE fp support files fpbasis.* added

---
+++ core/alsp_src/generic/fpbasis.c
@@ -0,0 +1,44 @@
+/*===========================================================================*
+ |              fpbasis.c
+ |      Copyright (c) 1996-97 Applied Logic Systems, Inc.
+ |
+ |      -- Floating point math abstractions
+ |
+ *===========================================================================*/
+#include "defs.h"
+#include "fpbasis.h"
+#ifdef MacOS
+#include <fp.h>
+#endif
+
+int is_ieee_nan PARAMS( (double) );
+int is_ieee_inf PARAMS( (double) );
+
+int
+is_ieee_nan(v)
+	double v;
+{
+	return isnan(v);
+}
+
+int
+is_ieee_inf(v)
+	double v;
+{
+#ifdef SOLARIS
+	switch (fpclass(v)) {
+	case FP_NINF:
+		return(1);
+	case FP_PINF:
+		return(1);
+	default:
+		return(0);
+	}
+#elif defined(MacOS)
+	return !isfinite(v);
+#elif (defined(__sgi) && defined(__mips))
+	return(!finite(v));
+#else
+	return isinf(v);
+#endif
+}
