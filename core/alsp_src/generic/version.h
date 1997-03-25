92577f7075570554929df3fe0a0a9a4caf0730e9
Ken Bowen <kbowen@applied-logic-systems.com>
1997-03-25 21:56:16 +0000

    Version change

--- core/alsp_src/generic/version.h
+++ core/alsp_src/generic/version.h
@@ -5,5 +5,5 @@
  *          Version number - Thu Apr 4 09:28:36 EST 1996
  *===============================================================*/
 #ifndef SysVersionNum
-#define SysVersionNum "2.08d dev-BETA"
+#define SysVersionNum "2.08dBETA"
 #endif /* SysVersionNum */
