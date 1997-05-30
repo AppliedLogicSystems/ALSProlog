5ece41e0c75cf49d16914c7e3b4e18e3878e48e1
Chuck Houpt <chuck@habilis.net>
1997-05-30 21:08:12 +0000

    moved from port/mswin32

---
+++ core/alsp_src/win32/mswin32_config.h
@@ -0,0 +1,34 @@
+/* 
+ *  mswin32_config.h	Hand made MSWin32 configuration file.
+ *	Copyright (c) 1996 Applied Logic Systems, Inc.
+ *
+ *  Author:	Chuck Houpt
+ *  Creation:	1/30/96
+ */
+
+#include "dfltsys.h"
+
+#define MSWin32 1
+#define OSStr "mswin32"
+
+#define HAVE_STDARG_H	1
+#define HAVE_STDLIB_H	1
+#define HAVE_FCNTL_H	1
+#define HAVE_STRING_H	1
+#define HAVE_SRAND		1
+
+#define HAVE_TIME		1
+
+#define HAVE_SOCKET		1
+#define BERKELEY_SOCKETS	1
+#define HAVE_SELECT		1
+
+#define PARAMREVBIT 1
+
+#define APP_PRINTF_CALLBACK	1
+
+#define HAVE_STRCSPN	1
+#define HAVE_STRSPN	1
+#define HAVE_STRTOK	1
+
+#define REVERSE_ENDIAN 1
---
+++ core/alsp_src/win32/mw_alspro_x86_prefix.h
@@ -0,0 +1,4 @@
+#include <Win32Headers>
+#define HAVE_CONFIG_H
+#define PORT 1
+#define MSWIN32 1
