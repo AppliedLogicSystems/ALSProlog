271ae2054fabfaccc1dde9736d5e59979f591ad2
Chuck Houpt <chuck@habilis.net>
1997-06-04 16:06:59 +0000

    New build projects and batch files

---
+++ core/alsp_src/win32/package.bat
@@ -0,0 +1,2 @@
+.\alspro_b -b -obp -g save_image('alspro.exe',[select_lib(builtins,[debugger])])
+.\demo_alspro_b -b -obp -g save_image('demo_alspro.exe',[select_lib(builtins,[debugger])])
---
+++ core/alsp_src/win32/testrun.bat
@@ -0,0 +1,1 @@
+alspro -obp -b %ALSP_SRC%\tests\autotest.pro %ALSP_SRC%\tests\atest_db.pro -g run_tests -p -srcdir %ALSP_SRC%
