7a73d97deae1f189e94dc3b57e61a30861cba631
Chuck Houpt <chuck@habilis.net>
1997-06-05 20:37:17 +0000

    An implementation of fpsetround for intel x86 using Metrowerks

---
+++ core/alsp_src/win32/x87_fpsetround.c
@@ -0,0 +1,25 @@
+#include "x87_fpsetround.h"
+
+typedef struct {
+   unsigned exception_masks:6;
+   unsigned reserved2:2;
+   unsigned percision:2;
+   unsigned rounding:2;
+   unsigned reserved1:4;
+} x87_control_word;
+
+fp_rnd fpsetround(fp_rnd mode)
+{
+    x87_control_word control;
+    fp_rnd old_rounding;
+
+    asm (fstcw control);
+
+    old_rounding = control.rounding;
+
+    control.rounding = mode;
+
+    asm (fldcw control);
+
+    return old_rounding;
+}
