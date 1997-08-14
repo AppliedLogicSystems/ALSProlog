4b196019845dcf0af80e1b77437d1674e526fd4f
Chuck Houpt <chuck@habilis.net>
1997-08-14 19:30:28 +0000

    Fix for virtual memory problems on AIX.  Basicly, memory mapped files were being placed in the space meant for the next big code block.  To solve this AIX allocates just one huge code block.

--- core/alsp_src/generic/alsmem.h
+++ core/alsp_src/generic/alsmem.h
@@ -21,7 +21,11 @@
 #define AM_MAXGLOBALS		64
 #else
 /* AM_BLOCKSIZE is the default block size to allocate */
+#ifdef _AIX
+#define AM_BLOCKSIZE		(0x00200000*0x10)	/* about 2 meg */
+#else
 #define AM_BLOCKSIZE		0x00200000	/* about 2 meg */
+#endif
 
 /* AM_MALLOCSPACE is the space we attempt to leave malloc with at 
    initialization */
