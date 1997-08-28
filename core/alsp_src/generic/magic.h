34b5a23f25257f39cc3b988627b783035e7dfa83
Chuck Houpt <chuck@habilis.net>
1997-08-28 16:19:49 +0000

    Fix for crashes involved different endian obp files by adding endian magic field.

--- core/alsp_src/generic/magic.h
+++ core/alsp_src/generic/magic.h
@@ -1,1 +1,2 @@
-#define MAGIC "ALS-Prolog Loadable Object Module\r\nFormat 1.21(port_threa,osYYYYYYYY)\r\n\032\004\019\026"
+#define MAGIC "ALS-Prolog Loadable Object Module\r\nFormat 1.22(port_threa,osYYYYYYYY)\r\n\032\004\019\026"
+#define ENDIAN_MAGIC 0x1234
