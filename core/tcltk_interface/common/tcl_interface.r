589afbc262f76c3f573ab27f2e3488acf40cb2bc
Chuck Houpt <chuck@habilis.net>
1997-10-23 21:34:36 +0000

    Version info for MacOS and Win32.

---
+++ core/tcltk_interface/common/tcl_interface.r
@@ -0,0 +1,9 @@
+#include <SysTypes.r>
+
+#include "version.h"
+
+resource 'vers' (1, purgeable) {
+	VERSION_MAJOR, VERSION_MINOR, release, 0, verUS,
+	VERSION_STRING,
+	VERSION_STRING " © Applied Logic Systems, Inc. 1997"
+};
