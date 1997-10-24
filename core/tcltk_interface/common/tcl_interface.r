8c2150104756e06d2a5b4470adf0c9cec987cab5
Chuck Houpt <chuck@habilis.net>
1997-10-24 16:51:51 +0000

    Added Version info for Unix, and made all versions 3-digit.

--- core/tcltk_interface/common/tcl_interface.r
+++ core/tcltk_interface/common/tcl_interface.r
@@ -3,7 +3,7 @@
 #include "version.h"
 
 resource 'vers' (1, purgeable) {
-	VERSION_MAJOR, VERSION_MINOR, release, 0, verUS,
+	VERSION_MAJOR, VERSION_MINOR << 4 | VERSION_PATCH, release, 0, verUS,
 	VERSION_STRING,
 	VERSION_STRING " © Applied Logic Systems, Inc. 1997"
 };
