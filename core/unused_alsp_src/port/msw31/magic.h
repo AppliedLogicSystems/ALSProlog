c97a651809974ef2ca7042c1f3b29367933b3dfc
Chuck Houpt <chuck@habilis.net>
1997-11-12 20:18:36 +0000

    removed old config files

--- core/unused_alsp_src/port/msw31/magic.h
+++
@@ -1,1 +0,0 @@
-#define MAGIC "ALS-Prolog Loadable Object Module\r\nFormat 1.21(port_threa,win3.1YYYY)\r\n\032\004\019\026"
--- core/unused_alsp_src/port/msw31/oconfig.h
+++
@@ -1,9 +0,0 @@
-/*=======================================================
- |		oconfig.h
- |		Copyright (c) 1995 Applied Logic Systems, Inc.
- |
- |		OS-specific config file for Microsoft Windows 3.1
- *======================================================*/
-
-#define MSW31	1
-#define OSStr "msw31"
