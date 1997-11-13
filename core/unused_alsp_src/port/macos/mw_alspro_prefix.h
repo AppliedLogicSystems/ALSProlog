99aa53e055f89f7073ba545bfde59b27d42a2775
Chuck Houpt <chuck@habilis.net>
1997-11-13 20:06:08 +0000

    Removed obsolete MacOS code.

--- core/unused_alsp_src/port/macos/mw_alspro_prefix.h
+++
@@ -1,3 +0,0 @@
-#include <MacHeaders.h>
-#include <ansi_prefix.mac.h>
-#define HAVE_CONFIG_H
--- core/unused_alsp_src/port/macos/oconfig.h
+++
@@ -1,9 +0,0 @@
-/*=======================================================
- |		oconfig.h
- |		Copyright (c) 1995 Applied Logic Systems, Inc.
- |
- |		OS-specific config file for MacOS
- *======================================================*/
-
-#define MacOS	1
-#define OSStr "mac"
