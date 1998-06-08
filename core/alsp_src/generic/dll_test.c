05d60eec44787aba3c221038d550ae34759fd638
Chuck Houpt <chuck@habilis.net>
1998-06-08 20:56:26 +0000

    DLL set to NOT load state from executable

--- core/alsp_src/generic/dll_test.c
+++ core/alsp_src/generic/dll_test.c
@@ -1,1 +1,1 @@
-#include "alspi.h"int main(int argc, char *argv[]){    return PI_main(argc, argv, NULL);}
+#include "alspi.h"int main(int argc, char *argv[]){    return PI_main(argc, argv, NULL);    PI_getan(NULL, NULL, 1);}
