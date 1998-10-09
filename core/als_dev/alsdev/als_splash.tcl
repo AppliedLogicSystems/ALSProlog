bb2821293357eb3650c599d0bc50d4638024776a
Chuck Houpt <chuck@habilis.net>
1998-10-09 22:47:09 +0000

    Added centering of splash and about box

--- core/als_dev/alsdev/als_splash.tcl
+++ core/als_dev/alsdev/als_splash.tcl
@@ -1,3 +1,26 @@
+proc center {window args} {
+	if {[llength $args]} then {
+		set x [winfo x $args]
+		set y [winfo y $args]
+		set ow [winfo width $args]
+		set oh [winfo height $args]
+	} else {
+		set x 0
+		set y 0
+		set ow [winfo screenwidth $window]
+		set oh [winfo screenheight $window]
+	} 
+	
+	set wratio 0.5
+	set hratio 0.3
+	
+	set w [winfo reqwidth $window]
+	set h [winfo reqheight $window]
+	
+	wm geometry $window \
+	+[expr round($ow*$wratio - $w/2)+$x]+[expr round($oh*$hratio - $h/2)+$y] 
+}
+
 proc splash {path} {
 	global tcl_platform
 	
@@ -12,7 +35,7 @@ proc splash {path} {
 	wm overrideredirect .als_splash_screen 1
 	label .als_splash_screen.label -image als_splash_gif -bd 1 -relief flat
 	pack .als_splash_screen.label -side top -expand 1 -fill both
-	wm geometry .als_splash_screen +270+200
+	center .als_splash_screen
 	wm deiconify .als_splash_screen
 	update idletasks
 }
