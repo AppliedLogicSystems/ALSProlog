20ba8aba50d0630fbb6f936ab8e10702d99d6233
Ken Bowen <kbowen@applied-logic-systems.com>
1998-12-05 14:58:01 +0000

    Scripts for settinging up alsdir in als_dev/alsdev in a way that can easily be updated from the main repository.

---
+++ core/als_dev/alsdev/update_all.sh
@@ -0,0 +1,10 @@
+cvs update
+cd alsdir
+cvs update
+cd images
+cp ../../images/*.* .
+cd ..
+cd shared
+cp ../../*.tcl .
+cp G:/ALS_Software/ALS_Packages/Tcl-Tk_Interface/Tcl-Tk_Interface_1.03/win32/*.* .
+cd ../..
