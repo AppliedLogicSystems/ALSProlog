1eb98a5d3695bd0f04447f6b76ee08d73effba71
Chuck Houpt <chuck@habilis.net>
1998-12-10 22:36:10 +0000

    Added more Tcl DLLs

--- core/win32/build.bat
+++ core/win32/build.bat
@@ -4,9 +4,10 @@ copy setcwd.bat temp.bat
 cd >> temp.bat
 call temp.bat
 del temp.bat
-echo %CWD%
 "%CWPATH%\bin\ide.exe" /b /t "Build All" "%CWD%\CWP4_win32_alsdev.mcp"
 copy "ALS Prolog Stub.exe" "ALS Prolog.exe"
+copy "%TCLTKPATH%\bin\msvcrt.dll" .
+copy "%TCLTKPATH%\bin\tclpip80.dll" .
 copy "%TCLTKPATH%\bin\tcl80.dll" .
 copy "%TCLTKPATH%\bin\tk80.dll" .
 xcopy /e /i "%TCLTKPATH%\lib" lib
@@ -16,4 +17,3 @@ copy ..\als_dev\alsdev\*.tcl alsdir\shared
 xcopy /e /i ..\als_dev\alsdev\images alsdir\images
 set DEV_ALSDIR=..\alsp_src
 alspro_b -b -g "(consult('..\\als_dev\\alsdev\\ldr_dvsh'), consult('..\\tcltk_interface\\common\\tcltk_util'), attach_image('ALS Prolog.exe'))"
-set DEV_ALSDIR=
