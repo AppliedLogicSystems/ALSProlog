6a53c88e213dd75af69f8f7dc5e73fb5171f9524
Chuck Houpt <chuck@habilis.net>
1998-12-10 23:17:20 +0000

    Initial directory build

--- installers/buildinstallers.bat
+++ installers/buildinstallers.bat
@@ -1,2 +1,16 @@
-This is a mod to the orginal
-test
+mkdir "ALS Prolog"
+copy "..\core\win32\ALS Prolog.exe" "ALS Prolog"
+copy ..\core\win32\msvcrt.dll "ALS Prolog"
+copy ..\core\win32\tcl80.dll "ALS Prolog"
+copy ..\core\win32\tk80.dll "ALS Prolog"
+copy ..\core\win32\tclpip80.dll "ALS Prolog"
+xcopy /e /i ..\core\win32\alsdir "ALS Prolog\alsdir"
+xcopy /e /i ..\core\win32\lib "ALS Prolog\lib"
+
+xcopy /e /i ..\examples "ALS Prolog\examples"
+
+mkdir "ALS Prolog\manual"
+
+xcopy /e /i ..\core\alsp_src\library "ALS Prolog\alsdir\library"
+
+
