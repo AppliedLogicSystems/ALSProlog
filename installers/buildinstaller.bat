bedda98910e6a9bb608bc6af126d51d0ece71601
Chuck Houpt <chuck@habilis.net>
1999-01-21 20:04:12 +0000

    Cleanup of dirs and installer

--- installers/buildinstaller.bat
+++ installers/buildinstaller.bat
@@ -1,3 +1,4 @@
+rmdir /q /s "ALS Prolog"
 mkdir "ALS Prolog"
 copy "..\core\win32\ALS Prolog.exe" "ALS Prolog"
 copy ..\core\win32\msvcrt.dll "ALS Prolog"
@@ -7,10 +8,12 @@ copy ..\core\win32\tclpip80.dll "ALS Prolog"
 xcopy /e /i ..\core\win32\alsdir "ALS Prolog\alsdir"
 xcopy /e /i ..\core\win32\lib "ALS Prolog\lib"
 
-xcopy /e /i ..\examples "ALS Prolog\examples"
+xcopy /i ..\examples\als\*.pro "ALS Prolog\examples\als"
+xcopy /i ..\examples\pxs\*.pro "ALS Prolog\examples\pxs"
 
 mkdir "ALS Prolog\manual"
 
-xcopy /e /i ..\core\alsp_src\library "ALS Prolog\alsdir\library"
+xcopy /i ..\core\alsp_src\library\*.pro "ALS Prolog\alsdir\library"
+xcopy /i ..\core\alsp_src\library\*.alb "ALS Prolog\alsdir\library"
 
 
