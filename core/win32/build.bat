642944d3d273d22e93fa3d633b41c17f306f64d6
Chuck Houpt <chuck@habilis.net>
1999-01-25 20:10:17 +0000

    Added student version

--- core/win32/build.bat
+++ core/win32/build.bat
@@ -6,6 +6,7 @@ call temp.bat
 del temp.bat
 "%CWPATH%\bin\ide.exe" /b /t "Build All" "%CWD%\CWP4_win32_alsdev.mcp"
 copy "ALS Prolog Stub.exe" "ALS Prolog.exe"
+copy "ALS Prolog Stub.exe" "ALS Student Prolog.exe"
 copy "%TCLTKPATH%\bin\msvcrt.dll" .
 copy "%TCLTKPATH%\bin\tclpip80.dll" .
 copy "%TCLTKPATH%\bin\tcl80.dll" .
@@ -19,5 +20,6 @@ copy ..\als_dev\alsdev\*.tcl alsdir\shared
 mkdir alsdir\images
 xcopy ..\als_dev\alsdev\images\*.gif alsdir\images
 set DEV_ALSDIR=..\alsp_src
-alspro_b -b -g "(consult('..\\als_dev\\alsdev\\ldr_dvsh'), consult('..\\tcltk_interface\\common\\tcltk_util'), attach_image('ALS Prolog.exe'))"
+alspro_b -b -g "(consult('..\\als_dev\\alsdev\\ldr_dvsh'),consult('..\\tcltk_interface\\common\\tcltk_util'), attach_image('ALS Prolog.exe'))"
+alspro_b -b -g "(consult('..\\als_dev\\alsdev\\ldr_dvsh'),consult('..\\tcltk_interface\\common\\tcltk_util'),builtins:abolish(save_image,1),builtins:abolish(save_image,2),sio:abolish(open_socket_stream,4),attach_image('ALS Student Prolog.exe'))"
 alspro_b -b -g "save_image(alspro)"
