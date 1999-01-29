515e7222ed09fdd20e66af1c31bb9c836bc88f2d
Chuck Houpt <chuck@habilis.net>
1999-01-29 20:23:42 +0000

    First Checked In.

---
+++ core/macos/build_student_image.c
@@ -0,0 +1,34 @@
+#include "alspi.h"
+
+static char *args[]= {
+	"alspro_b",
+	"mac_make.pro"
+};
+
+int main()
+{
+    return PI_main(2, args, NULL);
+}
+
+#if 0
+int main()
+{
+    PI_system_setup setup;
+
+	setup.heap_size = 0;
+    setup.stack_size = 0;
+    setup.icbuf_size = 0;
+    setup.alsdir = "::alsp_src:";
+    setup.saved_state = NULL;
+    setup.load_executable_state = 0;
+    setup.argc = 2;
+    setup.argv = NULL;
+
+    PI_set_console_functions(standard_console_read, standard_console_write,
+    				standard_console_error);
+
+ PI_startup(&setup);
+ PI_status_toplevel(&success);
+ PI_shutdown()
+}
+#endif
---
+++ core/macos/mac_make.pro
@@ -0,0 +1,19 @@
+:-
+	consult(
+		['::tcltk_interface:common:tcltk_util.pro',
+		 blt_dvsh, dbg_class, projects, 
+		':alsdir:library:listutl1',
+		':alsdir:library:miscterm',
+		':alsdir:library:msc_ioin',
+		':alsdir:library:strctutl',
+		':alsdir:library:strings',
+		':alsdir:library:tcl_sppt',
+		':alsdir:library:tk_alslib'
+		]).
+	
+%:- attach_image('ALS Prolog', [select_lib(builtins, [debugger]),
+%	select_lib(library,[miscterm,msc_ioin,strctutl,strings,tcl_sppt,
+%						tk_alslib, listutl1])]).
+
+
+:- attach_image('ALS Prolog PPC', []).
