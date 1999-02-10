399bb7a0311b154f9a8db1e370c2dde2a176dbaf
Chuck Houpt <chuck@habilis.net>
1999-02-10 14:23:30 +0000

    Cleanup and changed "update" to "update idletasks" to avoid processing possible open apple events.

--- core/als_dev/alsdev/als_splash.tcl
+++ core/als_dev/alsdev/als_splash.tcl
@@ -23,21 +23,17 @@ proc center {window args} {
 
 proc splash {path} {
 	global tcl_platform
-	
+
 	wm withdraw .
 	toplevel .als_splash_screen -bd 2 -relief flat
-	wm withdraw .als_splash_screen
 	if {$tcl_platform(platform) == "macintosh"} then {
 		image create photo als_splash_gif -format gif -data [resource read GIFf turnstile_splash]
 	} else {
 		image create photo als_splash_gif -file [file join $path .. images turnstile_splash.gif]
 	}
-	wm overrideredirect .als_splash_screen 1
+	wm overrideredirect .als_splash_screen true
 	label .als_splash_screen.label -image als_splash_gif -bd 1 -relief flat
 	pack .als_splash_screen.label -side top -expand 1 -fill both
-	update idletask
 	center .als_splash_screen
-	update idletask
-	wm deiconify .als_splash_screen
-	update
+	update idletasks
 }
