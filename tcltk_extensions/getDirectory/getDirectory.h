0d45e435496ce0e1a341b8dc60b642635447e1a6
Chuck Houpt <chuck@habilis.net>
1999-02-24 22:39:11 +0000

    Cleanup

--- tcltk_extensions/getDirectory/getDirectory.h
+++
@@ -1,5 +0,0 @@
-#include "tcl.h"
-#include "tk.h"
-
-int GetDirectory(Tcl_Interp *interp, Tcl_DString *initdir,
-	Tk_Window parent, const char *prompt);
--- tcltk_extensions/getDirectory/unix_pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-package ifneeded getDirectory 1.0 "package require Tk 8.0; [list source [file join $dir getDirectory.tcl]]"
-
