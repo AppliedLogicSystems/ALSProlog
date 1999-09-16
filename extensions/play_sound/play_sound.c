bb02e766488e0a27483ed9306c2dea3d63e0e3fd
Chuck Houpt <chuck@habilis.net>
1999-09-16 21:30:58 +0000

    Added Macintosh version of play sound

--- extensions/play_sound/play_sound.c
+++ extensions/play_sound/play_sound.c
@@ -4,6 +4,11 @@ static int play_sound_file(void)
 {
     PWord arg;
     int arg_type;
+#ifdef macintosh
+	FSSpec spec;
+	short l, ref;
+	#define MAX_PATH 256
+#endif
     char path[MAX_PATH];
 
     PI_getan(&arg, &arg_type, 1);
@@ -19,8 +24,20 @@ static int play_sound_file(void)
 		break;	
 	}
 
+#ifdef macintosh
+	l = strlen(path);
+	memcpy(path+1, path, l);
+	path[0] = l;
+	FSMakeFSSpec (0, 0, (unsigned char *)path, &spec);	
+	if (FSpOpenDF(&spec, fsRdPerm, &ref) != noErr) PI_FAIL;
+	if (SndStartFilePlay(NULL, ref, 0, NULL, NULL, NULL, NULL, FALSE) != noErr)
+		PI_FAIL;
+	FSClose(ref);
+	PI_SUCCEED;
+#elif WIN32
 	if (PlaySound(path, NULL, SND_FILENAME)) PI_SUCCEED;
 	else PI_FAIL;
+#endif
 }
 
 PI_BEGIN
