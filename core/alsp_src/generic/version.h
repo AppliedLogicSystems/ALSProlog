9686e76bd39a8137d776f0c6e5be124f6ba7b5af
Ken Bowen <kbowen@applied-logic-systems.com>
1999-10-27 12:55:03 +0000

    Up version to 3.1.8dev

--- core/alsp_src/generic/version.h
+++ core/alsp_src/generic/version.h
@@ -5,10 +5,10 @@
  *          Version number - Thu Apr 4 09:28:36 EST 1996
  *===============================================================*/
 
-#define VERSION_STRING "3.1.7dev"
+#define VERSION_STRING "3.1.8dev"
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 1
-#define VERSION_PATCH 6
+#define VERSION_PATCH 8
 
 /* 
 Win32 File Flag choices:
