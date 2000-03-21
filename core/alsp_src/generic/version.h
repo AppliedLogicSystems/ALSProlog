fb2609fd7e4483e22eefe7c01444b956f7752869
Ken Bowen <kbowen@applied-logic-systems.com>
2000-03-21 20:50:26 +0000

    Version 3.1.15

--- core/alsp_src/generic/version.h
+++ core/alsp_src/generic/version.h
@@ -5,10 +5,10 @@
  *          Version number - Thu Apr 4 09:28:36 EST 1996
  *===============================================================*/
 
-#define VERSION_STRING "3.1.14"
+#define VERSION_STRING "3.1.15"
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 1
-#define VERSION_PATCH 14
+#define VERSION_PATCH 15
 
 /* 
 Win32 File Flag choices:
