60c2ec4b05338364f74d4ceca4a1737288cd407b
Ken Bowen <kbowen@applied-logic-systems.com>
2015-10-19 00:59:09 +0000

    Moving the alsprolog web site (folder website) out of trunk and renaming folder to alsprolog.com

--- website/deploy_site.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eux
-
-cd ..; tar -cf ./alsprolog.com.tar ./alsprolog.com ; gzip ./alsprolog.com.tar; sh alsprolog.com/2bakha alsprolog.com.tar.gz ; rm alsprolog.com.tar.gz
--- website/install_site.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eux
-
-cd ..; mv ftp/alsprolog.com.tar.gz . ; gunzip ./alsprolog.com.tar.gz ; tar -xf ./alsprolog.com.tar ; rm ./alsprolog.com.tar
