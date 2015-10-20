fd649f7bcd278cb39929fb8020556d8a34594b31
Chuck Houpt <chuck@habilis.net>
2015-10-20 15:28:27 +0000

    Removed bulk of Mac OS Classic files Still remaining are code ifdefs, shared mac/win32 projects, build instructions, etc.

--- core/alsp_src/macos/cw_plugin_prefix.h
+++
@@ -1,1 +0,0 @@
-#include <MacHeaders.h>#include <ansi_prefix.mac.h>#define HAVE_CONFIG_H#define PORT 1#define MACOS 1#define CW_PLUGIN 1
--- core/alsp_src/macos/mw_alspro_prefix.h
+++
@@ -1,8 +0,0 @@
-#include <MacHeaders.h>
-/*#include <ansi_prefix.mac.h>*/
-#pragma once off
-#define HAVE_CONFIG_H
-#define PORT 1
-#define MACOS 1
-
-#include "prefix.h"
