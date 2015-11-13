e34bfb062798c9dbd36f7e63a60ed7658171ef08
kenbowen <kbowen@applied-logic-systems.com>
2015-11-13 15:15:59 -0700

    Modify [dist/super]clean so that toplevel ~unix does its own cleaning.

---
+++ unix/superclean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+echo "Doing superclean"
+case `uname -rs` in
+    "SunOS 4"*)	ARCH=sunos ;;
+    "SunOS 5"*)	ARCH=solaris ;;
+    Linux*) 	ARCH=linux ;;
+    "HP-UX"*)	ARCH=hpux ;;
+    "IRIX"*)	ARCH=irix ;;
+    "Darwin"*)	ARCH=darwin ;;
+    *) 		echo "Unknown machine type..."; exit 1 ;;
+esac
+	rm -rf *.tgz $ARCH
+
+
