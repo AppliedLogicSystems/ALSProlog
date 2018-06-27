e70619c451c309a502c071aebc295b7f680a8ec5
Chuck Houpt <chuck@habilis.net>
2018-06-27 11:22:28 -0400

    Add debug option to curl test script
    The curl tests will drop into the shell after running, if the debug environment flag is set to 1. Example via make:
    
    make curl_test debug=1

--- core/alsp_src/tests/tsuite/curl_test.sh
+++ core/alsp_src/tests/tsuite/curl_test.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+: ${debug:=0}
+
 ALSPRO=$1
 
 TESTDIR=$(dirname "$0")
@@ -10,4 +12,9 @@ trap 'pkill -P $$' EXIT
 
 php --server localhost:8888 --docroot "$TESTDIR"/echo &
 
+if (( $debug ))
+then
+"$ALSPRO" "$TESTDIR"/curl_test.pro -g 'test ; true'
+else
 "$ALSPRO" "$TESTDIR"/curl_test.pro -g test
+fi
