d21399a90dd376485273749983d13c06a77e5dfd
Chuck Houpt <chuck@habilis.net>
2018-06-30 08:50:29 -0400

    add doc

--- core/alsp_src/tests/tsuite/echo/index.php
+++ core/alsp_src/tests/tsuite/echo/index.php
@@ -1,15 +1,18 @@
 <?php
 
+// Set custom response code, if requested
 if (isset($_GET["r"])) {
 	http_response_code($_GET["r"]);
 	unset($_GET["r"]);
 }
 
+// Output server/request values, if requested
 if (count($_GET)) {
 	foreach ($_GET as $key => $value) {
 		echo isset($_SERVER[$key]) ? $_SERVER[$key] : $_REQUEST[$key];
 	}
 } else {
+	// echo non-empty input, or path
 	$input = file_get_contents('php://input');
 	if ($input) echo $input; else echo ltrim($_SERVER["PATH_INFO"], "/");
 }
