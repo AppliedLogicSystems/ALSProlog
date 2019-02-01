79b51bdfee02d980b75f86d94f46eae2f1fa94c4
kenbowen <kbowen@applied-logic-systems.com>
2019-02-01 10:03:10 -0700

    Mistakenly committed softlink; removed it and committing actual file.

--- docs/src_help_md/alshelp/searchhelp.js
+++ docs/src_help_md/alshelp/searchhelp.js
@@ -1,1 +1,13 @@
-../searchhelp.js
+
+// Trying just to find any entry with title containing "checks" ($exported_proc/3 starts with "checks"):
+$(function() {
+	$('#mysearch').on('search', function() {
+	$('.eentry').hide().filter(".eentry.find('.a[title~=checks'])").show();
+	})
+});
+
+// This works, including collapsing unselected entries (<li>'s),
+// but of course doesn't search the whole title (prev: ix) attribute, 
+// only the link content:
+//	$('.eentry').hide().filter('.eentry:contains("' + $(this).val() + '")').show();
+
