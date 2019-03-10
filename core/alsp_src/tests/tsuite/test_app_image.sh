85d58e9767293ff74dc4cb7c366f6c86b1647395
Chuck Houpt <chuck@habilis.net>
2019-03-09 20:51:44 -0500

    Fix image tests on windows
    - Made test script executable and removed sh invocation, so Bash is invoked via shebang mechanism.
    - Restore full Bash error reporting.
    - Abstract test script path and executable extension to cover both unix/win.
    - Strip newlines from captured output to avoid extra lines added under Cygwin.
    - Quote output expansion in tests to avoid false negatives fron test ([) when output has multiple words.

--- core/alsp_src/tests/tsuite/test_app_image.sh
+++ core/alsp_src/tests/tsuite/test_app_image.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 #
 
-set -eux
+set -Eeuxo pipefail
+
+export TESTDIR=$(dirname "$0")
+
+case "$(uname)" in
+    "CYGWIN"*)  EXT=.exe ;;
+    *)          EXT=''   ;;
+esac
 
 # Testing for save_image functionality on unix/linux/macOS
 #
-if [ -f "./app_image0" ]; then
-    rm -f app_image0 app_image0.pst 
+if [ -f "./app_image0$EXT" ]; then
+    rm -f app_image0$EXT app_image0$EXT.pst
 fi
-if [ -f "./app_image1" ]; then
-    rm -f app_image1 app_image1.pst 
+if [ -f "./app_image1$EXT" ]; then
+    rm -f app_image1$EXT app_image1$EXT.pst
 fi
-if [ -f "./app_image2" ]; then
-    rm -f app_image2 app_image2.pst 
+if [ -f "./app_image2$EXT" ]; then
+    rm -f app_image2$EXT app_image2$EXT.pst
 fi
 
-SIPATH=../../alsp_src/tests/tsuite/savedimages/
+SIPATH=$TESTDIR/savedimages
 
 echo "Begin saved images tests"
 
 # app_image0:
 TEST_C_OUT=$(./alspro $SIPATH/app_image_test0.pro -q -g mk_app_image0 -b)
 
-if [ $TEST_C_OUT != "app_image0_saved" ];
+if [ "${TEST_C_OUT//$'\r'}" != "app_image0_saved" ];
 then
     echo "  >>FAIL: app_image0_save_image error: GOT $TEST_C_OUT"
     exit 1
@@ -30,18 +37,18 @@ else
     echo "  >> app_image0_save_image ok"
 fi
 
-if [ ! -f "./app_image0" ]; then
+if [ ! -f "./app_image0$EXT" ]; then
     echo "  >>FAIL: app_image0 does not exist"
     exit 1
 fi
-if [ ! -f "./app_image0.pst" ]; then
+if [ ! -f "./app_image0$EXT.pst" ]; then
     echo "  >>FAIL: app_image0.pst does not exist"
     exit 1
 fi
 
 TEST_C_OUT=$(./app_image0)
 
-if [ $TEST_C_OUT != "app_image0_running" ];
+if [ "${TEST_C_OUT//$'\r'}" != "app_image0_running" ];
 then
     echo "  >>FAIL: app_image0_running error: GOT $TEST_C_OUT"
     exit 1
@@ -53,7 +60,7 @@ fi
 
 TEST_C_OUT=$(./alspro $SIPATH/app_image_test1.pro -q -g mk_app_image1 -b)
 
-if [ $TEST_C_OUT != "app_image1_saved" ];
+if [ "${TEST_C_OUT//$'\r'}" != "app_image1_saved" ];
 then
     echo "  >>FAIL: app_image1_saved error: GOT $TEST_C_OUT"
     exit 1
@@ -61,11 +68,11 @@ else
     echo "  >> app_image1_save_image ok"
 fi
 
-if [ ! -f "./app_image1" ]; then
+if [ ! -f "./app_image1$EXT" ]; then
     echo "  >>FAIL: app_image1 does not exist"
     exit 1
 fi
-if [ ! -f "./app_image1.pst" ]; then
+if [ ! -f "./app_image1$EXT.pst" ]; then
     echo "  >>FAIL: app_image1.pst does not exist"
     exit 1
 fi
@@ -74,7 +81,7 @@ fi
 
 TEST_C_OUT=$(./app_image1)
 
-if [ $TEST_C_OUT != "app_image1_making_app_image2" ];
+if [ "${TEST_C_OUT//$'\r'}" != "app_image1_making_app_image2" ];
 then
     echo "  >>FAIL: app_image1_made_app_image2 error: GOT $TEST_C_OUT"
     exit 1
@@ -82,18 +89,18 @@ else
     echo "  >> app_image1_made_app_image2 ok"
 fi
 
-if [ ! -f "./app_image2" ]; then
+if [ ! -f "./app_image2$EXT" ]; then
     echo "  >>FAIL: app_image2 does not exist"
     exit 1
 fi
-if [ ! -f "./app_image2.pst" ]; then
+if [ ! -f "./app_image2$EXT.pst" ]; then
     echo "  >>FAIL: app_image2.pst does not exist"
     exit 1
 fi
 
 TEST_C_OUT=$(./app_image2)
 
-if [ $TEST_C_OUT != "app_image2_running" ];
+if [ "${TEST_C_OUT//$'\r'}" != "app_image2_running" ];
 then
     echo "  >>FAIL: app_image2_running error: GOT $TEST_C_OUT"
     exit 1
