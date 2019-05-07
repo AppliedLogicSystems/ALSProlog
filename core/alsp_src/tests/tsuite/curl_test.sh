13a1d627c0dad2996416e7894e64064069ebbb5f
Chuck Houpt <chuck@habilis.net>
2019-05-07 08:37:52 -0400

    Disable OBP in test to avoid extraneous file creation.

--- core/alsp_src/tests/tsuite/curl_test.sh
+++ core/alsp_src/tests/tsuite/curl_test.sh
@@ -16,7 +16,7 @@ sleep 2
 
 if (( $debug ))
 then
-"$ALSPRO" -s "$TESTDIR" "$TSUITE"/curl_test.pro -g 'test ; true'
+"$ALSPRO" -no_obp -s "$TESTDIR" "$TSUITE"/curl_test.pro -g 'test ; true'
 else
-"$ALSPRO" -s "$TESTDIR" "$TSUITE"/curl_test.pro -g test < /dev/null
+"$ALSPRO" -no_obp -s "$TESTDIR" "$TSUITE"/curl_test.pro -g test < /dev/null
 fi
