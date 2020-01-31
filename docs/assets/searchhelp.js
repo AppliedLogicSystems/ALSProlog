07c2a21c00ca64e88d8f85ba128cf61efe6cff6e
kenbowen <kbowen@applied-logic-systems.com>
2020-01-31 11:36:55 -0700

    Repair errors, & control displays.

--- docs/assets/searchhelp.js
+++ docs/assets/searchhelp.js
@@ -1,27 +1,34 @@
+var switchpoint = 2;
+var rgbWhite = "rgb(255, 255, 255)";
+var rgbWhiteA = "rgba(0, 0, 0, 0)";
 
-function accessPredPage(id) 
+function accessPredPage(page, pred) 
 {
     var localSearchWhere = $("input[name='searchtype']:checked").val();
-
     var srchVal = $("#searchInput").val();
+    var cleanp;
 
-    var aid = "#"+id;
+    var pred0 = pred.charAt(0);
+    if (pred0 === "$") { 
+	cleanp = pred.slice(1); 
+    } else {
+	cleanp = pred;
+    }
+
+    var aid = "#"+page+"_"+encodeURIComponent(cleanp);
     var initurl = $(aid).attr('href');
 
     var xurl = initurl;
     if (srchVal  === '') {
         xurl = xurl + "?stype=" + localSearchWhere + "&stgt=";
     } else {
-        xurl = xurl + "?stype=" + localSearchWhere + "&stgt=" + "\\" + srchVal;
+        xurl = xurl + "?stype=" + localSearchWhere + "&stgt=" + srchVal;
     }
-
     $(aid).attr('href',xurl)
 }
 
 function setupRef()
 {
-//alert("setupRef: window.location.href=" + window.location.href);
-
     var locarray = window.location.href.split("?");
     if (locarray.length > 1) {
 	  var qs = locarray[locarray.length - 1];
@@ -33,8 +40,7 @@ function setupRef()
 	  var searchtype = stypeeq.split("=")[1];
 	  $("#"+searchtype).prop( "checked", true );
 
-	  var searchterm = searchtermeq.split("=")[1];
-	  searchterm = searchterm.substring(1);
+	  var searchterm = decodeURIComponent(searchtermeq.split("=")[1]);
 	  $("#searchInput").val(searchterm);
     } 
     doSearch();
@@ -45,35 +51,49 @@ $.fn.filterPreds = function(str, radioVal) {
         	// Usually iterate over the items and return for chainability:
         return this.each(function() {
              	// do something to each item matching the selector
-	   var rx = new RegExp(str);
-	   var str2 = '\^' + str;
-	   var rx2 = new RegExp(str2);
+	   var plain_x = new RegExp(str);
+	   var front_str = '\^\\' + str;
+	   var front_x = new RegExp(front_str);
+
 
 	   if (radioVal == "srchboth") {
-	       if (($(this).text().trim().match(rx) != null) ||
-                     ($(this).find(".dscr").text().trim().match(rx2) != null) ) 
+	       if (($(this).find("a").text().trim().match(plain_x) != null) ||
+                     ($(this).find(".dscr").text().trim().match(plain_x) != null) ) 
 	       {
                     $(this).find("a").css("background-color", "LightCyan")
-		    $(this).closest(".grp").find("details").find("summary").first().css("background-color", "LightPink")
+		    $(this).closest(".grp").find("details").find("summary").first().css("background-color", "#ffe2e6")
 		    $(this).closest(".pkg").find("details").find("summary").first().css("background-color", "AntiqueWhite")
+		    if (str.length > switchpoint) {
+		    	$(this).closest(".grp").find("details").attr('open', '')
+		    	$(this).closest(".pkg").find("details").attr('open', '')
+		    }
                } else {
                     $(this).hide()
                }
 	   } else if ((radioVal == "srchdesc")) {
-	       if ($(this).find(".dscr").text().trim().match(rx2) != null) 
+               if ($(this).find(".dscr").text().trim().match(plain_x) != null) 
 	       {
                     $(this).find("a").css("background-color", "LightCyan")
-		    $(this).closest(".grp").find("details").find("summary").first().css("background-color", "LightPink")
+		    $(this).closest(".grp").find("details").find("summary").first().css("background-color", "#ffe2e6")
 		    $(this).closest(".pkg").find("details").find("summary").first().css("background-color", "AntiqueWhite")
+		    if (str.length > switchpoint) {
+		    	$(this).closest(".grp").find("details").attr('open', '')
+		    	$(this).closest(".pkg").find("details").attr('open', '')
+		    }
                } else {
                     $(this).hide()
                }
 	   } else {
-	       if ($(this).find("a").text().trim().match(rx) != null) 
+		str = "^"+str;
+	       if ($(this).find("a").text().trim().match(plain_x) != null) 
 	       {
                     $(this).find("a").css("background-color", "LightCyan")
-		    $(this).closest(".grp").find("details").find("summary").first().css("background-color", "LightPink")
+		    $(this).closest(".grp").find("details").find("summary").first().css("background-color", "#ffe2e6")
 		    $(this).closest(".pkg").find("details").find("summary").first().css("background-color", "AntiqueWhite")
+		    if (str.length > switchpoint) {
+		    	$(this).closest(".grp").find("details").attr('open', '')
+		    	$(this).closest(".pkg").find("details").attr('open', '')
+		    }
                } else {
                     $(this).hide()
                }
@@ -82,19 +102,29 @@ $.fn.filterPreds = function(str, radioVal) {
 };
 
 function doSearch () {
-        var str0 = $("#searchInput").val();
-	var str = "\\" + str0;
-	var localSearchWhere = $("input[name='searchtype']:checked").val();
-
-	var As = $(".prd");
-	if (localSearchWhere != window.radioVal ) {
-		window.radioVal = localSearchWhere;
+        var str = $("#searchInput").val();
+    	var localSearchWhere = $("input[name='searchtype']:checked").val();
+	var char0 = str.charAt(0);
+	if (char0 == "$") { 
+		str = str.slice(1); }
+	if (str) {
+		var As = $(".prd");
         	As.filterPreds(str, localSearchWhere);
-	} else {
-        	As.filterPreds(str, window.radioVal);
+		cleanUp();
 	}
-        As.filterPreds(str, window.radioVal);
 };
+
+function cleanUp(){
+
+        $.each($(".grp").find("details"),
+                function( index, value ) {
+                   var grpbkg = $(value).find("summary").first().css("background-color");
+                   if (grpbkg === rgbWhite || grpbkg === rgbWhiteA ){
+                        $(value).closest(".grp").find("details").removeAttr("open");
+                   }
+                }
+        )
+}
 
 function clearSearch() {
 	var As = $(".prd");
@@ -102,7 +132,8 @@ function clearSearch() {
 	var Cs = $(".pkg");
 	As.show()
 	As.find("a").css("background-color", "white")
-	Bs.find("details").find("summary").css("background-color", "white")
-	Cs.find("details").find("summary").css("background-color", "white")
-	Bs.find("details").removeAttr("open");
+	Bs.find("details").removeAttr('open').find("summary").css("background-color", "white")
+	Cs.find("details").removeAttr('open').find("summary").css("background-color", "white")
+	Cs.find("details").first().attr('open', '');
 }
+
